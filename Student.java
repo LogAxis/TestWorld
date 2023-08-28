8d128251279c6f8558d3958c85f57629eeef5700
LogAxis <143334549+LogAxis@users.noreply.github.com>
2023-08-28 12:53:54 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -13,5 +13,56 @@ public class Student {
     private String studentId;
     private int marks;
 
-    // Constructors, getters, setters
+        public Student() {
+    }
+
+    public Student(Long id, String name, String surname, String studentId, int marks) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.studentId = studentId;
+        this.marks = marks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+    
+    
 }
